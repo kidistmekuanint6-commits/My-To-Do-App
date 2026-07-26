69ed686abff07610b4ae051e7d40af25d11ad4e9
kidistmekuanint6-commits <kidistmekuanint6@gmail.com>
2026-07-26 19:42:02 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,60 @@
-const CACHE_NAME = "todo-app-v1";
+const CACHE_NAME = "todo-app-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./style.css",
-    "./script.js"
+    "./script.js",
+    "./manifest.json",
+    "./app-icon.png"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(FILES_TO_CACHE);
-            })
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        }).then(() => {
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                return response || fetch(event.request);
+        fetch(event.request)
+            .then((response) => {
+
+                const responseClone =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+                    });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(
+                    event.request
+                );
             })
     );
 });
