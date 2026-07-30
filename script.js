0c07f78f294c8ef3a74c40bf64a91a345f9f340c
kidistmekuanint6-commits <kidistmekuanint6@gmail.com>
2026-07-30 13:18:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ VAPID PUBLIC KEY
 ========================================================= */
 
 const VAPID_PUBLIC_KEY =
-"BD-RtRMXFPHwxdOXyV5U9DymQBTDLJSP1M9vispNiaY1ZDXBYI6kkuig3cg-uZ1TqB-L0DTLGcI-T1EPP5ceNfc";
-"BD-RtRMXFPHwxdOXyV5U9DymQBTDLJSP1M9vispNiaY1ZDXBYI6kkuig3cg-uZ1TqB-L0DTLGcI-T1EPP5ceNfc";
+"BD-const VAPID_PUBLIC_KEY =
+"BD-RtRMXFPHwxdOXyV5U9DymQBTDLJSP1M9vispNiaY1ZDXBYI6kkuig3cg-uZ1TqB-L0DTLGcI-T1EPP5ceNfc";-uZ1TqB-L0DTLGcI-T1EPP5ceNfc";
 
 /* =========================================================
 APP DATA
